b0ea7042ef0d71a065d7136696ed78f287c3ffaf
Younes Charfaoui <mxcsyounes@gmail.com>
2020-04-17 22:01:49 +0100

    Adding

---
+++ app/app/src/main/java/com/mxcsyounes/com/MapsActivity.kt
@@ -0,0 +1,32 @@
+package com.mxcsyounes.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var googleMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        this.googleMap = googleMap
+        val sydney = LatLng(28.0339, 1.6596)
+        this.googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Algeria"))
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
